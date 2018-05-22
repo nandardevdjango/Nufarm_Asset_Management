c0b25be5f14bf4a107ad3018aa9b4e3f25907d5c
nkusnandar <nkusnandar@IDN2C18GC2>
2018-05-22 18:56:21 +0700

    karek saeutik bagian dialog entry na

--- N_Asset/NA_DataLayer/NA_Goods_Outwards_BR.py
+++ N_Asset/NA_DataLayer/NA_Goods_Outwards_BR.py
@@ -77,3 +77,77 @@ class NA_BR_Goods_Outwards(models.Manager):
 		totalRecords = row[0]
 		cur.close()
 		return (result,totalRecords)
+
+	def getBrandForOutwards(self,searchText,orderFields,sortIndice,pageSize,PageIndex,userName):
+		#get item from goods received
+		Query =  "DROP TEMPORARY TABLE IF EXISTS Temp_T_Receive_Outwards_" + userName
+		cur = connection.cursor()
+		cur.execute(Query)
+		Query =  "DROP TEMPORARY TABLE IF EXISTS Temp_T_History_Outwards_" + userName		
+		cur.execute(Query)
+		
+		#Query new items
+		Query = "CREATE TEMPORARY TABLE Temp_T_Receive_Outwards" + userName  + """ ENGINE=MyISAM AS (SELECT g.idapp,g.itemcode as fk_goods,g.goodsname,IFNULL(ngd.BrandName,g.BrandName) AS brandname,ngd.typeapp AS type,ngd.serialnumber, 'not yet used' as lastinfo,ngd.idapp as fk_receive, \
+					0 AS fk_outwards,0 as fk_lending,0 AS fk_return,0 AS fk_maintenance,0 AS fk_disposal,0 AS fk_lost FROM n_a_goods g INNER JOIN n_a_goods_receive ngr ON ngr.fk_goods = g.IDApp INNER JOIN n_a_goods_receive_detail ngd ON ngr.IDApp = ngd.FK_App \
+					WHERE NOT EXISTS(SELECT IDApp FROM n_a_goods_history WHERE fk_goods = ngr.fk_goods AND serialnumber = ngd.serialnumber)) """
+		cur.execute(Query)
+	    # Query get last trans in history 		
+		Query = "CREATE TEMPORARY TABLE Temp_T_History_Outwards" + userName  + """ ENGINE=MyISAM AS (SELECT gh.idapp,gh.fk_goods,gh.goodsname,gh.brandname,gh.type,gh.serialnumber, \
+                    CASE 
+                        WHEN (gh.fk_return IS NOT NULL) THEN (SELECT e.NIK FROM employee e INNER JOIN n_a_goods_return ngn ON ngn.fk_usedemployee = e.idapp WHERE ngn.idapp = gh.fk_return) \
+                        END AS fk_usedemployee
+                        CASE 
+                        WHEN (gh.fk_return IS NOT NULL) THEN (SELECT e.employee_name FROM employee e INNER JOIN n_a_goods_return ngn ON ngn.fk_usedemployee = e.idapp WHERE ngn.idapp = gh.fk_return) \
+                        END AS usedemployee,
+					CASE \
+						WHEN (gh.fk_maintenance IS NOT NULL) THEN (SELECT CONCAT('Maintenance by ', IFNULL(maintenanceby,''), ' ',	IFNULL(PersonalName,''), \
+							(CASE \
+								WHEN (isfinished = 1 AND issucced = 1) THEN (CONCAT(' Date Returned  ',DATE_FORMAT(enddate,'%d %B %Y'),' (goods is able to use)')) \
+								WHEN (isfinished = 1 AND issucced = 0) THEN (CONCAT(' Date Returned ',DATE_FORMAT(enddate,'%d %B %Y'),' (goods is unable to use)')) \
+								WHEN (isfinished = 0) THEN (CONCAT(' Date maintenance ',DATE_FORMAT(enddate,'%d %B %Y'),' (goods is still in maintenance)')) \
+								END)) FROM n_a_maintenance WHERE IDApp = gh.fk_maintenance) \
+						WHEN(gh.fk_lending IS NOT NULL) THEN((CASE \
+																WHEN ((SELECT `status` FROM n_a_goods_lending WHERE idapp = gh.fk_lending) = 'L') THEN 'good is still lent' \
+																ELSE ('goods is able to use') \
+																END)) \
+						WHEN(gh.fk_outwards IS NOT NULL) THEN 'goods is still in use by other employee' \
+						WHEN (gh.fk_return IS NOT NULL) THEN 'goods is able to use' \
+						WHEN (gh.fk_disposal IS NOT NULL) THEN 'goods is unabled to use(been disposed,auction,broken,etc)' \
+						WHEN (gh.fk_lost IS NOT NULL) THEN 'goods has lost' \
+						ELSE 'Unknown or uncategorized last goods position' \
+						END AS lastinfo,
+                        gh.fk_receive,gh.fk_outwards,gh.fk_lending,gh.fk_return,gh.fk_maintenance,gh.fk_disposal,gh.fk_lost  \
+						FROM(			\
+							SELECT g.idapp,g.itemcode as  fk_goods,g.goodsname,IFNULL(ngd.brandName,g.brandName) AS brandName,ngd.typeapp as 'type',ngd.serialnumber,ngd.idapp AS fk_receive,ngh.fk_outwards,ngh.fk_lending, \
+							ngh.fk_return,ngh.fk_maintenance,ngh.fk_disposal,ngh.fk_lost FROM \
+							n_a_goods g INNER JOIN n_a_goods_receive ngr ON g.idapp = ngr.fk_goods INNER JOIN n_a_goods_receive_detail ngd ON ngd.fk_app = ngr.idapp \
+							INNER JOIN n_a_goods_history ngh ON ngh.fk_goods = g.idapp AND ngh.serialnumber = ngd.serialnumber \
+							WHERE ngh.createddate = (SELECT Max(CreatedDate) FROM n_a_goods_history WHERE fk_goods = g.idapp AND serialnumber = ngd.serialnumber))gh
+                             )				
+				"""
+		cur.execute(Query)
+		strLimit = '300'
+		if int(PageIndex) <= 1:
+			strLimit = '0'
+		else:
+			strLimit = str(int(PageIndex)*int(pageSize))
+		#gabungkan jadi satu
+		Query = "CREATE TEMPORARY TABLE Temp_F_" + userName + """ ENGINE=MyISAM AS (SELECT * FROM \
+				(SELECT * FROM Temp_T_Receive_""" + userName + """ \
+					UNION \
+				 SELECT * FROM Temp_T_History_""" + userName + """\
+				 )C WHERE (goodsname LIKE %s OR brandname LIKE %s) OR (serialnumber = %s))"""
+		cur.execute(Query,['%'+searchText+'%','%'+searchText+'%',searchText])
+		if orderFields == '':
+			Query  = "SELECT * FROM Temp_F_" + userName + " ORDER BY brandname " + (" DESC" if sortIndice == "" else ' ' + sortIndice) + " LIMIT " + strLimit + "," + str(pageSize)	
+		else:
+			Query  = "SELECT * FROM Temp_F_" + userName + " ORDER BY " + orderFields + (" DESC" if sortIndice == "" else ' ' + sortIndice) + " LIMIT " + strLimit + "," + str(pageSize)				
+		cur.execute(Query)
+		result = query.dictfetchall(cur)
+
+		Query = "SELECT COUNT(*) FROM Temp_F_" + userName
+		cur.execute(Query)
+		row = cur.fetchone()
+		totalRecords = row[0]
+		cur.close()
+		return (result,totalRecords)
