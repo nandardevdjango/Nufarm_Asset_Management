ceedd75e6ab2519e3685abd39e89c6d924a6fa4e
nkusnandar <nkusnandar@IDN2C18GC2>
2018-07-01 20:06:12 +0700

    goods outwards

--- N_Asset/app/NA_Urls/Transactions/NA_Goods_Outwards_url.py
+++ N_Asset/app/NA_Urls/Transactions/NA_Goods_Outwards_url.py
@@ -5,7 +5,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
 	url(r'^$',NA_Goods_Outwards_View.NA_Goods_Outwards,name='GoodsOutwards'),
     url(r'^NA_Goods_Outwards_Search/$',NA_Goods_Outwards_View.NA_Goods_Outwards_Search,name='GoodsOutwardManager'),
-    url(r'^ShowEntry_Outwards/$',NA_Goods_Outwards_View.ShowEntry_Outwards,name='ShowEntryOutwards'),
+    url(r'^ShowEntry_Outwards',NA_Goods_Outwards_View.ShowEntry_Outwards,name='ShowEntryOutwards'),
     url(r'^getEmployee/$',NA_Goods_Receive_View.SearchEmployeebyform),#ambil funtion yang sudah ada di receive view
     url(r'^SearchEmployeebyform/$',RedirectView.as_view(url='/Transactions/Goods_Receive/IT/SearchEmployeebyform/',permanent=False,query_string=True),name='redirect-to-SearchEmployeebyform-from-outwards'),#ambil funtion yang sudah ada di receive view
     url(r'^getGoodsWithHistory/$',NA_Goods_Outwards_View.getGoodsWithHistory,name='getGoodsWithHistory_Outwards'),
