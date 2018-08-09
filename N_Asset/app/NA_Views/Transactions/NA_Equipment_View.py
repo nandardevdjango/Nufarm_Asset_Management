9a060363b9bf868e01013d63711c7272bd138356
rimba88 <rimba@88spares.com>
2018-08-09 23:30:51 +0700

    post add equipment

---
+++ N_Asset/app/NA_Views/Transactions/NA_Equipment_View.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+
+from django import forms
+from django.shortcuts import render
+from django.views.generic import View
+
+from NA_Models.models import NAGoodsEquipment
+from NA_DataLayer.common import Data, decorators, commonFunct
+
+
+class NAEquipmentForm(forms.Form):
+    name_app = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'NA-Form-Control',
+            'style': 'display:inline-block;width:215px;',
+            'placeholder': 'equipment name'
+        }
+    ))
+
+    def clean(self):
+        name_app = self.cleaned_data.get('name_app')
+        if name_app:
+            if NAGoodsEquipment.objects.filter(name_app__icontains=name_app).exists():
+                self.add_error('name_app', Data.Exists.value)
+        return super(NAEquipmentForm, self).clean()
+
+    def save(self, request):
+        equipment = NAGoodsEquipment()
+        equipment.name_app = self.cleaned_data.get('name_app')
+        equipment.type_app = NAGoodsEquipment.get_type_app(request)
+        equipment.createdby = request.user.username
+        equipment.createddate = datetime.now()
+        equipment.save()
+        return (Data.Success, )
+
+
+class NAEquipmentView(View):
+
+    def get(self, request):
+        form = NAEquipmentForm()
+        return render(request, 'app/Transactions/NA_Entry_Equipment.html', {'form': form})
+
+    def post(self, request):
+        form = NAEquipmentForm(request.POST)
+        if form.is_valid():
+            result = form.save(request=request)
+            return commonFunct.response_default(result)
+        else:
+            raise forms.ValidationError(form.errors)
