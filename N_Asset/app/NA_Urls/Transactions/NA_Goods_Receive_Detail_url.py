5a9d15f5658c14c0ecd2e653bf15a48e4caf2d41
nandardevdjango <nandar.dev_django@yahoo.com>
2018-10-01 23:36:38 +0700

    saeutik pisan

--- N_Asset/app/NA_Urls/Transactions/NA_Goods_Receive_Detail_url.py
+++ N_Asset/app/NA_Urls/Transactions/NA_Goods_Receive_Detail_url.py
@@ -1,5 +1,9 @@
 from django.conf.urls import url
-from app.NA_Views.Transactions import NA_Goods_Receive_Detail
+from app.NA_Views.Transactions import NA_Goods_Receive_Detail,NA_Goods_Receive_View
 urlpatterns = [
 		url(r'^$',NA_Goods_Receive_Detail.entrybatchdetail,name='entrybatchdetail'),
+        	url(r'^getBrandForDetailEntry/$',NA_Goods_Receive_View.getBrandForDetailEntry,name='getBrandForDetailEntry'),
+	url(r'^getTypeApps/$',NA_Goods_Receive_View.getTypeApps,name='getTypeApps'),
+    url(r'^deleteDetail/$',NA_Goods_Receive_View.deleteDetail,name='deleteDetail'),
+    url(r'^getData/$',NA_Goods_Receive_Detail.getData),
 	]
